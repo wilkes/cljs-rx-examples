cc477641f926c50a7d5ab9aea18bfde8a4c1700b
Wilkes Joiner <wilkesjoiner@gmail.com>
2012-12-16 20:55:21 -0600

    refactored paint

--- src/paint/core.cljs
+++ src/paint/core.cljs
@@ -1,45 +1,29 @@
 (ns paint.core
   (:require [cljs-rx.jquery :as rxj]
             [cljs-rx.observable :as rx]
-            [crate.core]
             [jayq.core :refer [$] :as j]
             [jayq.util :refer [log]])
   (:use-macros [crate.def-macros :only [defpartial]]))
 
-(def log-pr (comp log pr-str))
-
-(def $content ($ "#main-content"))
-
-(defpartial content []
-  [:canvas#canvas {:height 768 :width 1024}])
+(defn mouse-drag [target]
+  (let [mouse-up (rxj/mouseup target)
+        mouse-move (rxj/mousemove target)]
+    (rx/select-many (rxj/mousedown target)
+                    (fn [_]
+                      (-> mouse-move
+                          (rx/select (juxt #(.-offsetX %) #(.-offsetY %)))
+                          (rx/buffer-with-count 2 1)
+                          (rx/select (juxt first second))
+                          (rx/take-until mouse-up))))))
 
 (defn ^:export main []
-  (j/append $content (content))
+  (j/append ($ "#main-content")
+            "<canvas id=\"canvas\" height=\"768\" width=\"1024\"/>")
   (let [canvas ($ :#canvas)
         ctx (.getContext (first canvas) "2d")
-        mouse-diffs (-> canvas
-                        rxj/mousemove
-                        (rx/buffer-with-count 2 1)
-                        (rx/select (fn [x]
-                                     {:first (first x)
-                                      :second (second x)})))
-        mouse-button (rx/merge (-> canvas
-                                   rxj/mousedown
-                                   (rx/select (constantly true)))
-                               (-> canvas
-                                   rxj/mouseup
-                                   (rx/select (constantly false))))
-        paint (-> mouse-button
-                  (rx/select (fn [down]
-                               (if down
-                                 mouse-diffs
-                                 (rx/take mouse-diffs 0))))
-                  rx/switch-latest)]
-    (rx/subscribe paint (fn [x]
-                          (.moveTo ctx
-                                   (-> x :first .-offsetX)
-                                   (-> x :first .-offsetY))
-                          (.lineTo ctx
-                                   (-> x :second .-offsetX)
-                                   (-> x :second .-offsetY))
-                          (.stroke ctx)))))
+        drag (mouse-drag canvas)]
+    (rx/subscribe drag
+                  (fn [[[x1 y1] [x2 y2]]]
+                    (.moveTo ctx x1 y1)
+                    (.lineTo ctx x2 y2)
+                    (.stroke ctx)))))
